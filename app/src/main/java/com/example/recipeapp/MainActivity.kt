84bf9aef865856c419dd6d37687fd23f11b2e3d8
Nipun Bhagat <npabhagat38@gmail.com>
2021-07-25 16:20:36 +0530

    initializind app

---
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.recipeapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
