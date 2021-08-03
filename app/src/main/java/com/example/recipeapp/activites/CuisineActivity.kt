7d066bae13a8f05105f4654d138184350d9f99e7
Nipun Bhagat <npabhagat38@gmail.com>
2021-08-03 13:59:08 +0530

    cuisine activity initialized

---
+++ app/src/main/java/com/example/recipeapp/activites/CuisineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipeapp.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipeapp.R
+
+class CuisineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuisine)
+    }
+}
